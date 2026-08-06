244bff4a33b69647bc3248f1365f69f28b3c591b
Daniel Jesus <185440573+DanielWhite001@users.noreply.github.com>
2026-08-05 21:26:52 -0300

    codigo par ou impar

---
+++ ParOuImpar/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ParOuImpar;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int numero = 0;
+        bool numeroValido = false;
+        
+
+        while(!numeroValido){
+            Console.Write("olá, por favor digite um número!: ");
+            string digito = Console.ReadLine();
+            numeroValido = int.TryParse(digito, out numero);
+
+        if (!numeroValido) {
+         Console.WriteLine("Número inválido!");
+       }
+        }
+       
+       
+       if (numero % 2 == 0)
+        {
+            Console.WriteLine("O número digitado é par!");
+        }
+        else
+        {
+            Console.WriteLine("O número digitado é ímpar!");
+        }
+    }
+}
